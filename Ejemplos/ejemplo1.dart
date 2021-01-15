d61ae6f1caad5785c51292369257c15abdb3dada
Carlos <carlosf.soto1222@gmail.com>
2021-01-15 13:40:37 -0600

    subida del ejemplo 1

---
+++ Ejemplos/ejemplo1.dart
@@ -0,0 +1,11 @@
+// define una función
+
+imprimeEntero(int numero) {
+  print("El número es: $numero"); //imprime un numero en consola
+}
+
+//Método principal
+void main() {
+  var numero = 6989; //Declaramos einicializamos la variable
+  imprimeEntero(numero);
+}
